4b0360acdbcae7d1cc5110a7419a0c56a550d1a3
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-07-31 17:51:27 +0530

    fast2sms integration

--- src/controllers/consumer.controller.js
+++ src/controllers/consumer.controller.js
@@ -5,7 +5,8 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose";
-
+import axios from "axios";
+import otpGenerator from 'otp-generator';
 
 // const generateAccessAndRefereshTokens = async(userId) =>{
 //     try {
@@ -51,9 +52,6 @@ const registerConsumer = asyncHandler( async (req, res) => {
         throw new ApiError(409, "User with email or phoneNumber already exists")
     }
     //console.log(req.files);
-
-    
-
     const consumer = await Consumer.create({
         phoneNumber, email, gstin, type, companyName, website, pan
     })
@@ -72,6 +70,68 @@ const registerConsumer = asyncHandler( async (req, res) => {
 
 } )
 
+const sendConsumerOtp = asyncHandler(async (req, res) => {
+    try {
+        const otp = otpGenerator.generate(4, { upperCaseAlphabets: false, lowerCaseAlphabets: false, specialChars: false });
+
+        const cDate = new Date();
+        const phoneNumber = req.body.phoneNumber;
+        const existedUser = await Consumer.findOne({ phoneNumber })
+
+    if (existedUser) {
+        throw new ApiError(409, " phoneNumber already exists")
+    }
+        // sent otp on mobile number
+        await axios.get('https://www.fast2sms.com/dev/bulkV2', {
+            params: {
+                authorization: process.env.FAST2SMS_API_KEY,
+                variables_values: otp,
+                route: 'otp',
+                numbers: phoneNumber
+            }
+        });
+
+        // console.log('sent otp')
+        return res.status(201).json(
+            new ApiResponse(201, {otp}, "OTP sent successfully!"));
+    } catch (error) {
+        console.error('Error sending OTP:', error);
+        res.status(400).json(
+            // { success: false, message: 'Failed to send OTP.' }
+            new ApiResponse(400, {error}, "Failed to send OTP")
+        );
+    }
+})
+
+// const verifyConsumerOtp = async (req, res) => {
+//     try {
+
+//         const existingOtp = await otpModel.findOne({ mobileNumber: req.body.mobileNumber });
+//         console.log('existingOtpDetails', existingOtp);
+//         if (!existingOtp.otp) {
+//             throw new Error('Otp does not exists');
+//         }
+
+//         // otp expiration time is 5 minutes
+//         const cDate = new Date();
+
+//         if (cDate.getTime() > (existingOtp.otpExpiration + 300000)) {
+//             throw new Error('Otp expired');
+//         }
+
+//         if (existingOtp.otp != req.body.otp) {
+//             throw new Error('Invalid otp')
+//         }
+
+//         return res.status(201).json({ success: true })
+//     } catch (error) {
+//         return res.status(400).json({
+//             success: false,
+//             message: error?.message
+//         })
+//     }
+// }
+
 export {
-    registerConsumer
+    registerConsumer, sendConsumerOtp, 
 }
--- src/routes/consumer.routes.js
+++ src/routes/consumer.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerConsumer } from "../controllers/consumer.controller.js";
+import { registerConsumer, sendConsumerOtp, } from "../controllers/consumer.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -9,6 +9,7 @@ const router = Router()
 
 
 router.route("/signup").post(registerConsumer)
+router.route("/sendOtp").get(sendConsumerOtp)
 
 
 
