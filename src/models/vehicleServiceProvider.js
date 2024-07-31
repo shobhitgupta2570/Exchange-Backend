432ee7b3418b705d1ab2eaca7505193653754527
nitish1899 <nitishcyberminds@gmail.com>
2024-07-31 18:20:59 +0530

    added relations

--- src/models/vehicleServiceProvider.js
+++ src/models/vehicleServiceProvider.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 
+// Define the VehicleServiceProvider schema
 const vehicleServiceProviderSchema = new Schema(
     {
         aadhar: {
@@ -22,12 +23,28 @@ const vehicleServiceProviderSchema = new Schema(
             required: [true, "Pan is required"],
             uppercase: true,
             unique: true,
-        }
+        },
+        // References to owners for a broker
+        owners: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "VehicleServiceProvider"
+        }],
+        // References to drivers and brokers for an owner
+        drivers: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "VehicleServiceProvider",
+            required: false
+        }],
+        brokers: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "VehicleServiceProvider",
+            required: false
+        }]
     },
     {
         timestamps: true
     }
-)
+);
 
-
+// Create a model from the schema
 export const VehicleServiceProvider = mongoose.model("VehicleServiceProvider", vehicleServiceProviderSchema);
